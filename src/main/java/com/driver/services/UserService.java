e21c0bfa73bdfeaf198b13f170cfb9a5c9b28568
Jayavardhan-J <Jayavardhanjj3184@gmail.com>
2023-07-29 14:07:29 +0530

    Solution added

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -23,19 +23,36 @@ public class UserService {
 
 
     public Integer addUser(User user){
-
-        //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        User savedUser=userRepository.save(user);
+        return savedUser.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
-
+        User user = userRepository.findById(userId).get();
+        Subscription subscription=user.getSubscription();
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
+        int count=findSeriesCount(user,subscription.getSubscriptionType());
 
-
-        return null;
+        return count;
     }
+    public int findSeriesCount(User user,SubscriptionType subscriptionType){
+        int count=0;
+        for(WebSeries webSeries: webSeriesRepository.findAll()){
+            if (user.getAge() >= webSeries.getAgeLimit()) {
+                if (subscriptionType.equals(SubscriptionType.ELITE)) {
+                    count++;
+                } else if (subscriptionType.equals(SubscriptionType.PRO)){
+                    if (webSeries.getSubscriptionType().equals(SubscriptionType.BASIC) || webSeries.getSubscriptionType().equals(SubscriptionType.PRO))
+                        count++;
+                } else {
+                    if (webSeries.getSubscriptionType().equals(SubscriptionType.BASIC))
+                        count++;
+                }
+            }
+        }
 
+        return count;
+    }
 
 }
